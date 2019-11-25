7229edd41a09b45e6225466788861b768383819d
Youssef Assem <yo_90_32@hotmail.com>
2019-11-26 00:20:39 +0200

    whole workspace
    Add workspace contaning usb_dev_mouse to make it easier for importing.
    
    Steps to import in code composer:
    project -> import ccs projects ->browse to main directory ->select all ->tick copy projects -> finish

---
+++ tm4c123g/usb_dev_mouse/Debug/usb_dev_mouse.d
@@ -0,0 +1,67 @@
+# FIXED
+
+usb_dev_mouse.obj: ../usb_dev_mouse.c
+usb_dev_mouse.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h
+usb_dev_mouse.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h
+usb_dev_mouse.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h
+usb_dev_mouse.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h
+usb_dev_mouse.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h
+usb_dev_mouse.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h
+usb_dev_mouse.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h
+usb_dev_mouse.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h
+usb_dev_mouse.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_sysctl.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/fpu.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/grlib/grlib.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/grlib/widget.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usblib.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usbhid.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdevice.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdevicepriv.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdhid.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdhidmouse.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/examples/boards/dk-tm4c123g/drivers/cfal96x64x16.h
+usb_dev_mouse.obj: C:/ti/TivaWare_C_Series-2.1.4.178/examples/boards/dk-tm4c123g/drivers/buttons.h
+usb_dev_mouse.obj: ../usb_mouse_structs.h
+
+../usb_dev_mouse.c: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/fpu.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/systick.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/grlib/grlib.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/grlib/widget.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usblib.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usbhid.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdevice.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdevicepriv.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdhid.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdhidmouse.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/examples/boards/dk-tm4c123g/drivers/cfal96x64x16.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/examples/boards/dk-tm4c123g/drivers/buttons.h: 
+../usb_mouse_structs.h: 
