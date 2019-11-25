665a2e0438651bf625f82faf1a77add92010eb44
Youssef Assem <yo_90_32@hotmail.com>
2019-11-25 22:58:14 +0200

    Start for usb
    A start for the usb mouse program that builds.

---
+++ usb_dev_mouse/Debug/drivers/buttons.d
@@ -0,0 +1,37 @@
+# FIXED
+
+drivers/buttons.obj: C:/ti/TivaWare_C_Series-2.1.4.178/examples/boards/dk-tm4c123g/drivers/buttons.c
+drivers/buttons.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h
+drivers/buttons.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h
+drivers/buttons.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h
+drivers/buttons.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h
+drivers/buttons.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h
+drivers/buttons.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h
+drivers/buttons.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h
+drivers/buttons.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h
+drivers/buttons.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h
+drivers/buttons.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+drivers/buttons.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+drivers/buttons.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom_map.h
+drivers/buttons.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+drivers/buttons.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+drivers/buttons.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+drivers/buttons.obj: C:/ti/TivaWare_C_Series-2.1.4.178/examples/boards/dk-tm4c123g/drivers/buttons.h
+
+C:/ti/TivaWare_C_Series-2.1.4.178/examples/boards/dk-tm4c123g/drivers/buttons.c: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom_map.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/examples/boards/dk-tm4c123g/drivers/buttons.h: 
---
+++ usb_dev_mouse/Debug/usb_mouse_structs.d
@@ -0,0 +1,43 @@
+# FIXED
+
+usb_mouse_structs.obj: ../usb_mouse_structs.c
+usb_mouse_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h
+usb_mouse_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h
+usb_mouse_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h
+usb_mouse_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h
+usb_mouse_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h
+usb_mouse_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h
+usb_mouse_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h
+usb_mouse_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h
+usb_mouse_structs.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h
+usb_mouse_structs.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+usb_mouse_structs.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/usb.h
+usb_mouse_structs.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usblib.h
+usb_mouse_structs.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usbhid.h
+usb_mouse_structs.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usb-ids.h
+usb_mouse_structs.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdevice.h
+usb_mouse_structs.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdevicepriv.h
+usb_mouse_structs.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdhid.h
+usb_mouse_structs.obj: C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdhidmouse.h
+usb_mouse_structs.obj: ../usb_mouse_structs.h
+
+../usb_mouse_structs.c: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/usb.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usblib.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usbhid.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/usb-ids.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdevice.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdevicepriv.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdhid.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/usblib/device/usbdhidmouse.h: 
+../usb_mouse_structs.h: 
