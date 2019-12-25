202d9aa43ab82e2b3d1a8812792c72830c157cd0
InEdited <ineditedofficial@gmail.com>
2019-12-25 22:08:17 +0200

    Many updates hardware side

---
+++ aaaaaaaaaaaaaaah/main.c
@@ -0,0 +1,96 @@
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_memmap.h"
+
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+#include "utils/uartstdio.c"
+
+
+
+
+void
+InitConsole(void)
+{
+    //
+    // Enable GPIO port A which is used for UART0 pins.
+    // TODO: change this to whichever GPIO port you are using.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+
+    //
+    // Configure the pin muxing for UART0 functions on port A0 and A1.
+    // This step is not necessary if your part does not support pin muxing.
+    // TODO: change this to select the port/pin you are using.
+    //
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+
+    //
+    // Enable UART0 so that we can configure the clock.
+    //
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+
+    //
+    // Use the internal 16MHz oscillator as the UART clock source.
+    //
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+
+    //
+    // Select the alternate (UART) function for these pins.
+    // TODO: change this to select the port/pin you are using.
+    //
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    //
+    // Initialize the UART for console I/O.
+    //
+    UARTStdioConfig(0, 115200, 16000000);
+    UARTprintf("Initialized Console \n");
+}
+/**
+ * main.c
+ */
+int main(void)
+{
+    char cThisChar;
+
+    InitConsole();
+    SysCtlPeripheralReset(SYSCTL_PERIPH_UART1);
+    SysCtlPeripheralReset(SYSCTL_PERIPH_GPIOB);
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART1);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+
+    GPIOPinConfigure(GPIO_PB0_U1RX);
+    GPIOPinConfigure(GPIO_PB1_U1TX);
+
+    GPIOPinTypeUART(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    UARTConfigSetExpClk(UART1_BASE, SysCtlClockGet(), 115200,
+                        (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE |
+                         UART_CONFIG_PAR_NONE));
+    UARTprintf("hana5od data now");
+    do
+    {
+        //
+        // Read a character using the blocking read function.  This function
+        // will not return until a character is available.
+        //
+        cThisChar = UARTCharGet(UART1_BASE);
+
+        //
+        // Write the same character using the blocking write function.  This
+        // function will not return until there was space in the FIFO and
+        // the character is written.
+        //
+        //UARTCharPut(UART0_BASE, cThisChar);
+        UARTprintf("%c",cThisChar);
+    }
+    while(1);
+	return 0;
+}
